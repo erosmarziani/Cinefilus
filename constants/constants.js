1f781b6c9a0b3d94a974da576b6a2c7d96eb4158
alenchufacundo <alenfacurios@gmail.com>
2024-09-19 22:18:20 -0300

    divido en carpetas

--- constants/constants.js
+++
@@ -1,8 +0,0 @@
-// Tu API Key, esto deberia ser personal de cada, luego lo cambio al .env para que se commitee y cada uno lo tengo en su compu.
-const API_KEY = '008dc80f7978770f2a8420b4a573fbab'
-const BASE_URL = 'https://api.themoviedb.org/3/movie/popular'
-
-module.exports = {
-  BASE_URL,
-  API_KEY
-}
