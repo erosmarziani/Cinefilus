641e0e9266e8dec564f5b14c2d886802de5fb0b4
alenchufacundo <alenfacurios@gmail.com>
2024-09-23 19:17:50 -0300

    agrego gitignore y api key en variables de enterno

--- back/constants/constants.js
+++ back/constants/constants.js
@@ -1,5 +1,5 @@
 // Tu API Key, esto deberia ser personal de cada, luego lo cambio al .env para que se commitee y cada uno lo tengo en su compu.
-const API_KEY = '008dc80f7978770f2a8420b4a573fbab'
+const API_KEY = process.env.API_KEY;
 const BASE_URL = 'https://api.themoviedb.org/3'
 
 module.exports = {
