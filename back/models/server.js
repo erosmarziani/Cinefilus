b0e583c91b8f8254efabbc50f226c6434e6b7961
cosba2 <marcosbayon22@gmail.com>
2025-01-03 21:04:06 -0300

    actualizo repo con cors

--- back/models/server.js
+++ back/models/server.js
@@ -1,31 +1,37 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 
 class Server {
-  constructor () {
-    this.app = express()
-    this.port = process.env.PORT || 3000
-    this.middleware()
-    this.rutas()
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || 3000;
+
+    // Llamar a los métodos de configuración
+    this.middleware();
+    this.rutas();
   }
 
-  middleware () {
-    this.app.use(express.static('public'))
+  middleware() {
+    // Habilitar CORS para todas las rutas
+    this.app.use(cors());
+
+    // Middleware para servir contenido estático
+    this.app.use(express.static('public'));
   }
 
-  // 3 paso.
-  // definimos la ruta -> es la que va ir en el navegador ->/peliculas
-  rutas () {
-    this.app.use('/popular', require('../routes/popular')) // Rios Facundo
-    this.app.use('/puntuados', require('../routes/puntuados')) // Bayon Marcos
-    this.app.use('/api/v1/tv', require('../routes/tvShows')) // Hernandez Joaquin
-    this.app.use('/upcoming', require('../routes/upcoming')) // Eros Marziani
+  rutas() {
+    // Rutas para diferentes endpoints
+    this.app.use('/popular', require('../routes/popular')); // Rios Facundo
+    this.app.use('/puntuados', require('../routes/puntuados')); // Bayon Marcos
+    this.app.use('/api/v1/tv', require('../routes/tvShows')); // Hernandez Joaquin
+    this.app.use('/upcoming', require('../routes/upcoming')); // Eros Marziani
   }
 
-  listen () {
+  listen() {
     this.app.listen(this.port, () => {
-      console.log(`La API está escuchando en el puerto ${this.port}`)
-    })
+      console.log(`La API está escuchando en el puerto ${this.port}`);
+    });
   }
 }
 
-module.exports = Server
+module.exports = Server;
